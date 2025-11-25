3b9f773ad59dab15d0b44d9a9c0b923744ad004b
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2025-11-25 16:30:24 -0500

    Move all previews to test/components/previews (ViewComponent default)
    Moved all preview files from spec/components/previews/ to test/components/previews/
    which is the default location ViewComponent looks for previews.
    
    Removed custom initializer and autoload path configs.
    
    All 11 previews should now load in Lookbook after server restart.

--- config/initializers/view_component.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.configure do
-  # ViewComponent configuration
-  config.view_component.preview_paths << Rails.root.join("spec/components/previews") if Rails.env.development?
-end

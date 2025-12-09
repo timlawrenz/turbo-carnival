7c5e83afb948bf20da588f41d854a7d6f446e9f5
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2025-12-09 18:36:42 -0500

    Fix gap_analyses request specs to use correct nested routes

--- spec/requests/gap_analyses_spec.rb
+++ spec/requests/gap_analyses_spec.rb
@@ -1,24 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe "GapAnalyses", type: :request do
+  let(:persona) { Persona.create!(name: "Test Persona") }
+
   describe "GET /index" do
     it "returns http success" do
-      get "/gap_analyses/index"
+      get persona_gap_analyses_path(persona)
       expect(response).to have_http_status(:success)
     end
   end
 
   describe "GET /show" do
     it "returns http success" do
-      get "/gap_analyses/show"
+      gap_analysis = GapAnalysis.create!(
+        persona: persona, 
+        analyzed_at: Time.current,
+        coverage_data: []
+      )
+      get persona_gap_analysis_path(persona, gap_analysis)
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /create" do
-    it "returns http success" do
-      get "/gap_analyses/create"
-      expect(response).to have_http_status(:success)
+  describe "POST /create" do
+    it "returns http redirect" do
+      post persona_gap_analyses_path(persona)
+      expect(response).to have_http_status(:redirect)
     end
   end
 
